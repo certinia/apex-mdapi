33c9afc60edef386cd45f5d6cee78e70e2813de4
Nate Pepper <maythesforcebewithyou@gmail.com>
2023-10-29 13:37:17 -0700

    Working, but does not expand on first click

--- sfdx-project/force-app/main/default/lwc/metadataBrowser/metadataBrowser.js
+++ sfdx-project/force-app/main/default/lwc/metadataBrowser/metadataBrowser.js
@@ -55,7 +55,12 @@ export default class MetadataBrowser extends LightningElement {
     }
     
     handleToggle(event) {
-        console.log(JSON.stringify(event.detail, null, 4));
+        console.log(`event.detail: ${JSON.stringify(event.detail, null, 4)}`);
+        const mdType = event.detail.name;
+        const treeGrid = this.template.querySelector('lightning-tree-grid');
+        const currentExpandedRows = treeGrid.getCurrentExpandedRows();
+        console.log(`currentExpandedRows: ${JSON.stringify(currentExpandedRows, null, 4)}`);
+        
         // event.detail:
         // {
         //     "name": "ApexClass",
@@ -69,31 +74,50 @@ export default class MetadataBrowser extends LightningElement {
         // }
         if(!event.detail.row.hasCalledApi) {
             console.log(`I haven't called the Metadata API yet for this metadataType, I'm doin' it!!`);
-            const mdType = event.detail.name;
             console.log(`mdType: ${mdType}`);
             retrieveListMetadata({ metadataTypeName: mdType })
             .then(result => {
                 const listMetaResult = JSON.parse(result);
                 console.log(`List Metadata Result: ${JSON.stringify(listMetaResult, null, 4)}`);
                 // Find gridData node, set hasCalledApi to true, and populate it with children
-                const idxCurrentNode = this.gridData.findIndex(node => node.name === mdType);
+                const idxCurrentNode = this.gridData.findIndex(node => {
+                    // console.log(`node: ${JSON.stringify(node, null, 4)}`);
+                    return node.metadataType === mdType;
+                });
+                console.log(`idxCurrentNode: ${idxCurrentNode}`);
                 const updatedNode = { ...this.gridData[idxCurrentNode] };
                 updatedNode.hasCalledApi = true;
-                console.log(`updatedNode: ${JSON.stringify(updatedNode, null, 4)}`);
-                const childrenArray = listMetaResult.map(result => result.fullName);
+                const childrenArray = listMetaResult.map(result => {
+                    return {
+                        metadataType: result.fullName,
+                        hasCalledApi: true,
+                        inFolder: false
+                    };
+                });
+                console.log(`childrenArray: ${JSON.stringify(childrenArray, null, 4)}`);
                 updatedNode._children = childrenArray;
-                this.gridData[idxCurrentNode] = updatedNode;
+                const updatedGridData = [...this.gridData];
+                console.log(`updatedNode: ${JSON.stringify(updatedNode, null, 4)}`);
+                updatedGridData[idxCurrentNode] = updatedNode;
+                console.log(`updatedGridData[idxCurrentNode]: ${JSON.stringify(updatedGridData[idxCurrentNode], null, 4)}`);
+                this.gridData = [...updatedGridData];
+                
+                const setCurrentExpandedRows = new Set(currentExpandedRows);
+                if(setCurrentExpandedRows.has(mdType)) {
+                    // If toggled node was already in expanded rows, remove
+                    setCurrentExpandedRows.delete(mdType);
+                } else {
+                    // If toggled node was not already in expanded rows, add
+                    setCurrentExpandedRows.add(mdType);
+                }
+                this.expandedRows = Array.from(setCurrentExpandedRows);
             })
             .catch(error => {
                 console.error(`List Metadata Error: ${JSON.stringify(error, null, 4)}`);
             });
         }
-        if(!event.detail.isExpanded) {
-            const idxCurrentNode = this.gridData.findIndex(node => node.name === event.detail.name);
-            const updatedNode = { ...this.gridData[idxCurrentNode] };
-            updatedNode.isExpanded = true;
-            this.gridData[idxCurrentNode] = updatedNode;
-        }
+        
+        
     }
     
     handleRowSelection(event) {
